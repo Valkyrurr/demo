232450e723b149a147ff915d361c636e420fafdc
KOKO <efstorralba@gmail.com>
2019-08-13 13:17:18 +0800

    seeded initial user that is available app-wide

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,13 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+
+        // TODO: this will conflict with TenantController (User::find(1)), overwriting values
+        // is likely not to happen if User model uses 'tenant' connection instead
+        $user = new App\User();
+        $user->name = 'Administrator';
+        $user->password = Hash::make('123456');
+        $user->email = 'web@master.com';
+        $user->save();
     }
 }
